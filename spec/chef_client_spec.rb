8fab3f18f7ff1998381307c81de39f8ce45d8a78
Doc Walker <doc.walker@jameshardie.com>
2014-01-05 09:46:23 -0600

    Cleanup block closure comments

--- spec/chef_client_spec.rb
+++ spec/chef_client_spec.rb
@@ -9,6 +9,6 @@ describe 'logrotate_::chef_client' do
       .with_content('/var/log/chef/client.log')
     expect(chef_run.template(file).owner).to eq('root')
     expect(chef_run.template(file).group).to eq('root')
-  end # it 'should create file owned by root:root'
+  end # it
 
-end # describe 'logrotate_::chef_client'
+end # describe
