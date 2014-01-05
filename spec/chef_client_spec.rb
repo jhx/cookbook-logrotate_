62dbda984be71dd14618cf70d3917287a1e304f8
Doc Walker <doc.walker@jameshardie.com>
2014-01-05 09:44:53 -0600

    Fix #7 add support for chefspec

--- spec/chef_client_spec.rb
+++ spec/chef_client_spec.rb
@@ -1,16 +1,14 @@
-require 'chefspec'
-require 'librarian/chef/integration/knife'
+require 'spec_helper'
 
 describe 'logrotate_::chef_client' do
-  opts = { :cookbook_path => Librarian::Chef.install_path.to_s }
-  let (:chef_run) do
-    ChefSpec::ChefRunner.new(opts).converge 'logrotate_::chef_client'
-  end # let (:chef_run)
-  
+  let(:chef_run) { ChefSpec::Runner.new.converge(described_recipe) }
+
   it 'should create file owned by root:root' do
     file = '/etc/logrotate.d/chef_client'
-    chef_run.should create_file_with_content file, '/var/log/chef/client.log'
-    chef_run.template(file).should be_owned_by 'root', 'root'
+    expect(chef_run).to render_file(file)
+      .with_content('/var/log/chef/client.log')
+    expect(chef_run.template(file).owner).to eq('root')
+    expect(chef_run.template(file).group).to eq('root')
   end # it 'should create file owned by root:root'
-  
+
 end # describe 'logrotate_::chef_client'
