6382730e7468e1427f71f8eb39c3107cece91ce0
Doc Walker <doc.walker@jameshardie.com>
2014-01-05 20:58:30 -0600

    Fix #18 streamline spec for file ownership

--- spec/chef_client_spec.rb
+++ spec/chef_client_spec.rb
@@ -6,10 +6,10 @@ describe 'logrotate_::chef_client' do
 
   it 'should create file owned by root:root' do
     file = '/etc/logrotate.d/chef_client'
+    expect(chef_run).to create_template(file)
+      .with(:owner => 'root', :group => 'root')
     expect(chef_run).to render_file(file)
       .with_content('/var/log/chef/client.log')
-    expect(chef_run.template(file).owner).to eq('root')
-    expect(chef_run.template(file).group).to eq('root')
   end # it
 
 end # describe
