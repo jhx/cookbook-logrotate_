16cc008c9a00d801ebc18f263f17f085862f2215
Doc Walker <doc.walker@jameshardie.com>
2014-02-23 07:24:28 -0600

    Fix #46 update unit tests; use single expectation

--- spec/chef_client_spec.rb
+++ spec/chef_client_spec.rb
@@ -3,16 +3,47 @@ require 'spec_helper'
 
 describe 'logrotate_::chef_client' do
   let(:chef_run) { ChefSpec::Runner.new.converge(described_recipe) }
-  let(:file) { '/etc/logrotate.d/chef_client' }
 
-  it 'creates file owned by root:root' do
-    expect(chef_run).to create_template(file)
-      .with(:owner => 'root', :group => 'root')
-  end # it
+  describe '/etc/logrotate.d/chef_client' do
+    it 'creates template with expected owner, group, mode' do
+      expect(chef_run).to create_template(subject)
+        .with(:owner => 'root', :group => 'root', :mode => '0644')
+    end # it
 
-  it 'renders file with expected content' do
-    expect(chef_run).to render_file(file)
-      .with_content('/var/log/chef/client.log')
-  end # it
+    it 'renders file with expected path' do
+      expect(chef_run).to render_file(subject)
+        .with_content('/var/log/chef/client.log')
+    end # it
+
+    it 'renders file with expected frequency' do
+      expect(chef_run).to render_file(subject)
+        .with_content('weekly')
+    end # it
+
+    it 'renders file with expected rotate limit' do
+      expect(chef_run).to render_file(subject)
+        .with_content('rotate 12')
+    end # it
+
+    it 'renders file with expected options (missingok)' do
+      expect(chef_run).to render_file(subject)
+        .with_content('missingok')
+    end # it
+
+    it 'renders file with expected options (compress)' do
+      expect(chef_run).to render_file(subject)
+        .with_content('compress')
+    end # it
+
+    it 'renders file with expected options (delaycompress)' do
+      expect(chef_run).to render_file(subject)
+        .with_content('delaycompress')
+    end # it
+
+    it 'renders file with expected postrotate command' do
+      expect(chef_run).to render_file(subject)
+        .with_content('service chef-client reload')
+    end # it
+  end # describe
 
 end # describe
