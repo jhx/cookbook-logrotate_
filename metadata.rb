32d2135318096029c81559ee904851d3499c0961
Doc Walker <doc.walker@jameshardie.com>
2014-02-24 19:44:12 -0600

    Fix #48 update encoding comment

--- metadata.rb
+++ metadata.rb
@@ -1,4 +1,4 @@
-# coding: utf-8
+# encoding: utf-8
 name              'logrotate_'
 maintainer        'James Hardie Building Products, Inc.'
 maintainer_email  'doc.walker@jameshardie.com'
--- test/integration/logrotate_/serverspec/chef_client_spec.rb
+++ test/integration/logrotate_/serverspec/chef_client_spec.rb
@@ -1,4 +1,4 @@
-# coding: utf-8
+# encoding: utf-8
 require 'spec_helper'
 
 describe 'logrotate_::chef_client' do
